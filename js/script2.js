f821ed72cefb2c160a3cfb18d764af0296f737c6
Kazi Sadman Shihab <kazinahin123@gmail.com>
2024-08-29 16:08:44 +0600

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+
+    var options = {
+
+        // enable autoplay
+        autoPlay: true,
+    
+        // autoplay interval in ms
+        autoPlayInterval: 3000,
+    
+        // min swipe distance in px
+        swipeThreshold: 50,
+        
+    };
+
+});
